8dbf47ed4b086e9b3896e98784cc8911686c6418
CommandMaker <command.maker83@gmail.com>
2021-02-05 21:34:37 +0100

    Premier commit et tâches avec GulpJS

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const { src, dest, watch, series } = require('gulp');
+const cssMin = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+function cssMinify() {
+    return src('res/css/**/*.css')
+        .pipe(concat('all.min.css'))
+        .pipe(cssMin({ compatibility: 'ie8' }))
+        .pipe(dest('res/minify/css'))
+}
+
+// JavaScript Task
+function jsTask() {
+    return src('res/js/*.js', { sourcemaps: true })
+        .pipe(concat('all.min.js'))
+        .pipe(terser())
+        .pipe(dest('res/minify/js/', { sourcemaps: '.' }));
+}
+
+function browsersyncServe(cb) {
+    browsersync.init({
+        proxy: "localhost",
+        notify: false,
+    });
+    cb();
+}
+
+function browsersyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
+
+function watchTask() {
+    watch('**/*.php', browsersyncReload);
+    watch(['css/**/*.css', 'res/js/**/*.js'], series(cssMinify, jsTask, browsersyncReload));
+}
+
+exports.dev = series(
+    cssMinify,
+    jsTask,
+    browsersyncServe,
+    watchTask
+);
+exports.cssMinify = cssMinify;
+exports.jsMinify = jsTask;
+exports.browsersync = browsersyncServe;
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Accueil - Meast</title>
+        <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/latest/css/pro.min.css"/>
+    </head>
+    <body>
+        <i class="fas fa-acorn"></i>
+        <p>Hello</p>
+    </body>
+</html>
